c0abee0c19952d18e74f8caf079c1e02de693537
Min ByeongUk <phraust@kaist.ac.kr>
2016-07-15 16:08:50 +0900

    Update training.h

--- training.h
+++ training.h
@@ -4,34 +4,38 @@
 #include "dataread.h"
 #include "pdlerror.h"
 #include <time.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <thread>
 #define DELTADEFAULT	1.0
 #define LAMBDADEFAULT	0.003
 #define HDEFAULT		0.0001
+
 
 class CTraining
 {
 private:
 	// N is the number of training sets
 	// and Nt is the number of test sets
-	int alpha, N, Nt, *D, count, l, learningSize;
+	int alpha, N, Nt, *D, count, l, learningSize, loaded;
 	// H, DELTA, LAMBDA are hyperparameters
 	// dW, db each stands for ds/dW, ds/db matrices
-	double H, ***W, **b, **s, *****dW, ***dLdW, ****db, **dLdb, L, Lold, DELTA, LAMBDA;
-	// this delta is a deltachronical matrix, used at ReLU
-	bool **delta;
+	double H, ***W, **b, *****dW, ***dLdW, ****db, **dLdb, L, Lold, DELTA, LAMBDA;
 	time_t starttime, endtime;
 	CDataread *pData;
 	void ParamAllocate();
+	CKeyinter Key;
 public:
+	
 	CTraining(CDataread* pD);
 	~CTraining();
 	int WeightInit(int size);
 	int WeightLoad();
-	void Training();
+	void Training(int threads);
 	void FileSave();
 	void ShowHelp();
+	void TrainingThreadFunc(int l);
 	int SetHyperparam(ValidationParam validateMode, double hyperparam);
 	double CheckAccuracy();
 };
